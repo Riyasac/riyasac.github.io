5a0d790fde4d631725f912a52b37b0e230aa7197
Riyas Ac <riyasac6@gmail.com>
2022-04-17 14:53:35 +0530

    Remove version

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -157,14 +157,6 @@ jQuery(document).ready(function($){
 });
 
 
-/*!
- * Validator v0.11.5 for Bootstrap 3, by @1000hz
- * Copyright 2016 Cina Saffary
- * Licensed under http://opensource.org/licenses/MIT
- *
- * https://github.com/1000hz/bootstrap-validator
- */
-
 +function ($) {
   'use strict';
 
